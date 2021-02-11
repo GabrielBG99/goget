cd59cea871cc18a4fc96103b9a57d3ccccce8f5a
Gabriel Baptistucci Garcia <gabrielbgarcia0999@gmail.com>
2021-02-11 18:05:38 -0300

    version command

--- main.go
+++ main.go
@@ -8,10 +8,19 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
+var Version = "dev"
+
 func main() {
+	cli.VersionFlag = &cli.BoolFlag{
+		Name:    "version",
+		Aliases: []string{"V"},
+		Usage:   "Show goget1s version",
+	}
+
 	app := &cli.App{
-		Name:  "goget",
-		Usage: "A download accelerator written in Go",
+		Name:    "goget",
+		Usage:   "A download accelerator written in Go",
+		Version: Version,
 		Commands: []*cli.Command{
 			commands.Single(),
 		},
